33fdeaaf643a3147ee7807e4eefe0dc1ccced144
Chaitanya Soni <c.soni@icloud.com>
2020-04-18 05:46:30 +0530

    Basic Navigation and Cocoapods
    Added RealmSwift and TLPhotoPicker.
    
    Created basic navigation template

---
+++ Test/Test/AttendanceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AttendanceViewController.swift
+//  Test
+//
+//  Created by Chaitanya Soni on 18/04/20.
+//  Copyright © 2020 Chaitanya Soni. All rights reserved.
+//
+
+import UIKit
+
+class AttendanceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Test/Test/GalleryViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GalleryViewController.swift
+//  Test
+//
+//  Created by Chaitanya Soni on 18/04/20.
+//  Copyright © 2020 Chaitanya Soni. All rights reserved.
+//
+
+import UIKit
+
+class GalleryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
